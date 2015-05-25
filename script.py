c5a23019d0dffb8de640c138a05cb48315e03d93
Dan Loewenherz <dloewenherz@gmail.com>
2015-05-25 16:09:40 -0500

    another change

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # vim: set fileencoding=utf-8 :
 
 from requests_oauthlib import OAuth1
-import restmapper
+from restmapper import RestMapper
 import os
 
 APP_KEY = os.environ['APP_KEY']
@@ -9,7 +9,7 @@ APP_SECRET = os.environ['APP_SECRET']
 OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
 
-Twitter = restmapper.RestMapper("https://api.twitter.com/1.1/", url_transformer=lambda url: url + ".json")
+Twitter = RestMapper("https://api.twitter.com/1.1/", url_transformer=lambda url: url + ".json")
 auth = OAuth1(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 twitter = Twitter(auth=auth)
 
