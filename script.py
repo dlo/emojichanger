f73959c7e9c7e5bceaac75d77e93aff8e7e95e60
Dan Loewenherz <dan@lionheartsw.com>
2016-04-17 10:57:41 -0500

    update script

--- script.py
+++ script.py
@@ -9,13 +9,11 @@ APP_KEY = os.environ['APP_KEY']
 APP_SECRET = os.environ['APP_SECRET']
 OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
-BIO_FORMAT = os.environ.get('BIO_FORMAT')
+BIO_FORMAT = unicode(os.environ.get('BIO_FORMAT'))
 
 Twitter = RestMapper("https://api.twitter.com/1.1/", url_transformer=lambda url: url + ".json")
 auth = OAuth1(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 twitter = Twitter(auth=auth)
-
-print BIO_FORMAT
 
 emojis=u"""☁☺☹✊✋☝✌⚡✨⭕❌⭐❕❔❗❓❄☀⛅☔⛄☎➿✂⚽⚾⛳♠♥♣♦☕♤♡♢♧⏰⏳⌛⌚♨✏✒✉⚓⛪⛺⛲⛵✈⛽⚠⛔⬆⬇⬅➡↗↖↘↙◀▶⏪⏩♿㊙㊗✳✴♈♉♊♋♌♍♎♏♐♑♒♓⛎⭕❌©®™⏫⏬↕↔↩↪⤴⤵ℹ❎Ⓜ⚫⚪◼◻▪▫✖➕➖➗➰〰♻☢☣☠☤⚕⚚†☯⚖☮⚘⚔☭⚒⚛⚜☥✠✙✞✟✧⋆★☆✪✫✬✭✮✯✰✡☫☬☸✵❂⚘❀❃❁✼♫♪☃❅❆☂❦♕♛♔♖♜☾→⇒⟹⇨⇰➩➪➫➬➭➮➯➲➳➵➸➻➺➼➽☜☟➹➷↶↷✆⌘⎋⏎⏏⎈⎌⍟❥ツღ☻"""
 
@@ -23,7 +21,7 @@ random_emoji = unicode(random.choice(emojis))
 if BIO_FORMAT is None:
     description = u"{}".format(random_emoji)
 else:
-    description = BIO_FORMAT + " " + unicode(random_emoji)
+    description = u"{} {}".format(BIO_FORMAT, random_emoji)
 
 response = twitter.POST.account.update_profile(description=description, parse_response=False)
 
