de9b3ee41672b65fefe24c48787cd5da8f518c72
Dan Loewenherz <dan@lionheartsw.com>
2016-04-17 11:01:01 -0500

    update bio format

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ APP_KEY = os.environ['APP_KEY']
 APP_SECRET = os.environ['APP_SECRET']
 OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
-BIO_FORMAT = unicode(os.environ.get('BIO_FORMAT').encode("utf8"))
+BIO_FORMAT = unicode(os.environ.get('BIO_FORMAT').decode("utf8"))
 
 Twitter = RestMapper("https://api.twitter.com/1.1/", url_transformer=lambda url: url + ".json")
 auth = OAuth1(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
